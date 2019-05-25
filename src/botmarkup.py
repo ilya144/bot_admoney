99a3c60c7099074f967b730349134a52e4094f45
ilya144 <ils12rus@gmail.com>
2019-05-25 20:49:08 +0300

    some refactor and main markup

--- src/botmarkup.py
+++ src/botmarkup.py
@@ -1,73 +1,116 @@
 import telebot
 
+class MarkupTree:
+
+    @staticmethod
+    def __reply_markup(buttons: list) -> telebot.types.ReplyKeyboardMarkup:
+        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
+        markup = telebot.types.ReplyKeyboardMarkup(True, False)
+        for row in buttons:
+            if type(row) == str:
+                markup.row(row)
+            else:
+                markup.row(*row)
+        return markup
+
+    @staticmethod
+    def __inline_markup(buttons: list) -> telebot.types.InlineKeyboardMarkup:
+        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
+        markup = telebot.types.InlineKeyboardMarkup()
+        for row in buttons:
+            if type(row) == str:
+                markup.row(row)
+            else:
+                markup.row(*row)
+        return markup
+
+    @classmethod
+    def get_reply_markup(cls, button_name: str) -> telebot.types.ReplyKeyboardMarkup:
+        buttons = cls.markup_dict[button_name]
+        markup = cls.__reply_markup(buttons)
+        return markup
+
+    @classmethod
+    def get_inline_markup(cls, button_name: str) -> telebot.types.InlineKeyboardMarkup:
+        buttons = cls.markup_dict[button_name]
+        markup = cls.__inline_markup(buttons)
+        return markup
+
+
 ######################################################################
 ###                                                                ###
 ###                   И С П О Л Н И Т Е Л Ь                        ###
 ###                                                                ###
 ######################################################################
 
-class ExecutorTree:
+class ExecutorTree(MarkupTree):
     # -- reply --
-    # u'Мой профиль'
+
+    main = [
+        ('Мой профиль', 'Задания')
+        ('Главное меню')
+    ]
+
     my_profile = [
         ('Соц сети', 'Баланс', 'Подписка'),
         ('Партнерская программа'),
         ('Назад', 'Главное меню')
     ]
-    # u'Задания'
+
     tasks = [
         ('Инстаграм'),
         ('ВК'),
         ('Телеграм'),
         ('Назад', 'Главное меню')
     ]
-    # u'Соц сети'
+
     social_media = tasks[:]
-    # u'Баланс'
+
     balance = [
         ('Статистика', 'Вывод средств'),
         ('Назад', 'Главное меню')
     ]
     # -- inline -- but not yet by now
-    # u'Подписка'
+
     subscribe = [
         ('Инстаграм', 'ВК', 'Телеграм'),
         ('Назад', 'Главное меню')
     ]
-    # u'Инстаграм' set
+
     instagram_settings = [
         ('Ввести ник', 'Поменять ник'),
         ('Назад', 'Главное меню')
     ]
-    # u'ВК' set
+
     vk_settings = [
         ('Ввести ID или ник', 'Поменять ID или ник'),
         ('Назад', 'Главное меню')
     ]
-    # u'Телеграм' set
+
     telegram_settings = [
         ('Подтвердите, что вы не бот'),
         ('Назад', 'Главное меню')
     ]
-    # u'Инстаграм' task
+
     instagram_tasks = [
         ('Поставить лайк', 'Подписаться'),
         ('Прокомментировать'),
         ('Назад', 'Главное меню')
     ]
-    # u'ВК' task
+
     vk_tasks = [
         ('Поставить лайк', 'Подписаться'),
         ('Прокомментировать', 'Зарепостить'),
         ('Назад', 'Главное меню')
     ]
-    # u'Телеграм' task
+
     telegram_tasks = [
         ('Подписаться', 'Посмотреть пост'),
         ('Назад', 'Главное меню')
     ]
 
     markup_dict = {
+        'Я Исполнитель': main,
         'Мой профиль': my_profile,
         'Задания': tasks,
         'Соц сети': social_media,
@@ -80,35 +123,6 @@ class ExecutorTree:
         'ВК && Задания': vk_tasks,
         'Телеграм && Задания': telegram_tasks,
     }
-    def _reply_markup(self, buttons: list) -> telebot.types.ReplyKeyboardMarkup:
-        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
-        markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        for row in buttons:
-            if type(row) == str:
-                markup.row(row)
-            else:
-                markup.row(*row)
-        return markup
-
-    def _inline_markup(self, buttons: list) -> telebot.types.InlineKeyboardMarkup:
-        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
-        markup = telebot.types.InlineKeyboardMarkup()
-        for row in buttons:
-            if type(row) == str:
-                markup.row(row)
-            else:
-                markup.row(*row)
-        return markup
-
-    def get_reply_markup(self, button_name: str) -> telebot.types.ReplyKeyboardMarkup:
-        buttons = self.markup_dict[button_name]
-        markup = self._reply_markup(buttons)
-        return markup
-
-    def get_inline_markup(self, button_name: str) -> telebot.types.InlineKeyboardMarkup:
-        buttons = self.markup_dict[button_name]
-        markup = self._inline_markup(buttons)
-        return markup
 
 
 ######################################################################
@@ -117,62 +131,75 @@ class ExecutorTree:
 ###                                                                ###
 ######################################################################
 
-class CustomerTree:
+class CustomerTree(MarkupTree):
+
+    main = [
+        ('Мой профиль', 'Баланс')
+        ('Партнерская программа')
+        ('Задания', 'Цены')
+        ('Главное меню')
+    ]
 
     my_profile = [
         ('Соц сети'),
         ('Назад', 'Главное меню')
     ]
+
     tasks = [
         ('Инстаграм'),
         ('ВК'),
         ('Телеграм'),
         ('Назад', 'Главное меню')
     ]
+
     social_media = tasks[:]
+
     balance = [
         ('Пополнить баланс'),
         ('Статистика'),
         ('Назад', 'Главное меню')
     ]
+
     prices = [
         ('Инстаграм', 'ВК', 'Телеграм'),
         ('Назад', 'Главное меню')
     ]
-    # u'Инстаграм' set
+
+
     instagram_settings = [
         ('Ввести ник', 'Поменять ник'),
         ('Назад', 'Главное меню')
     ]
-    # u'ВК' set
+
     vk_settings = [
         ('Ввести ID или ник', 'Поменять ID или ник'),
         ('Назад', 'Главное меню')
     ]
-    # u'Телеграм' set
+
     telegram_settings = [
         ('Подтвердите, что вы не бот'),
         ('Назад', 'Главное меню')
     ]
-    # u'Инстаграм' task
+
     instagram_tasks = [
         ('Поставить лайк', 'Подписаться'),
         ('Прокомментировать'),
         ('Назад', 'Главное меню')
     ]
-    # u'ВК' task
+
     vk_tasks = [
         ('Поставить лайк', 'Подписаться'),
         ('Прокомментировать', 'Зарепостить'),
         ('Назад', 'Главное меню')
     ]
-    # u'Телеграм' task
+
     telegram_tasks = [
         ('Подписаться', 'Посмотреть пост'),
         ('Назад', 'Главное меню')
     ]
 
     markup_dict = {
+        'Я Заказчик': main,
         'Мой профиль': my_profile,
         'Задания': tasks,
         'Соц сети': social_media,
@@ -185,35 +212,3 @@ class CustomerTree:
         'ВК && Задания': vk_tasks,
         'Телеграм && Задания': telegram_tasks,
     }
-
-    def _reply_markup(self, buttons: list) -> telebot.types.ReplyKeyboardMarkup:
-        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
-        markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        for row in buttons:
-            if type(row) == str:
-                markup.row(row)
-            else:
-                markup.row(*row)
-        return markup
-
-    def _inline_markup(self, buttons: list) -> telebot.types.InlineKeyboardMarkup:
-        " buttons looks like [[one, two],[one_on_next_row, ...], ...]"
-        markup = telebot.types.InlineKeyboardMarkup()
-        for row in buttons:
-            if type(row) == str:
-                markup.row(row)
-            else:
-                markup.row(*row)
-        return markup
-
-    def get_reply_markup(self, button_name: str) -> telebot.types.ReplyKeyboardMarkup:
-        buttons = self.markup_dict[button_name]
-        markup = self._reply_markup(buttons)
-        return markup
-
-    def get_inline_markup(self, button_name: str) -> telebot.types.InlineKeyboardMarkup:
-        buttons = self.markup_dict[button_name]
-        markup = self._inline_markup(buttons)
-        return markup
-
-
